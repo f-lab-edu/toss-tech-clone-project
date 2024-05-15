e1d83d2b919d7de5e19c016b08f84f2af0efe8b0
ohgus <123801385+ohgus@users.noreply.github.com>
2024-05-15 22:56:57 +0900

    [refactor]: fetch를 담당하는 함수와 아티클 리스트를 생성하는 함수를 분리 (#16)

--- src/app.js
+++ src/app.js
@@ -25,37 +25,43 @@ const start = () => {
 
 // document.addEventListener('DOMContentLoaded', start);
 
+const createArticleListItem = (articles) => {
+  const list = document.getElementById('article-list');
+
+  articles.forEach((article, index) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-item');
+    listItem.setAttribute('data-navigation', '');
+
+    const anchor = document.createElement('a');
+    anchor.setAttribute('data-navigation', '');
+    anchor.setAttribute('href', `/articles/${index}`);
+
+    const listTitle = document.createElement('h3');
+    listTitle.textContent = article.title;
+
+    const subTitle = document.createElement('p');
+    subTitle.textContent = article.subTitle;
+
+    const dateCreated = document.createElement('span');
+    dateCreated.textContent = article.date;
+
+    anchor.appendChild(listTitle);
+    anchor.appendChild(subTitle);
+    anchor.appendChild(dateCreated);
+
+    listItem.appendChild(anchor);
+    list.appendChild(listItem);
+  });
+};
+
 const fetchData = () => {
   fetch('data.json')
     .then((response) => response.json())
     .then((data) => {
       const { articles } = data;
-      const list = document.getElementById('article-list');
-      articles.forEach((article, index) => {
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-item');
-        listItem.setAttribute('data-navigation', '');
 
-        const anchor = document.createElement('a');
-        anchor.setAttribute('data-navigation', '');
-        anchor.setAttribute('href', `/articles/${index}`);
-
-        const listTitle = document.createElement('h3');
-        listTitle.textContent = article.title;
-
-        const subTitle = document.createElement('p');
-        subTitle.textContent = article.subTitle;
-
-        const dateCreated = document.createElement('span');
-        dateCreated.textContent = article.date;
-
-        anchor.appendChild(listTitle);
-        anchor.appendChild(subTitle);
-        anchor.appendChild(dateCreated);
-
-        listItem.appendChild(anchor);
-        list.appendChild(listItem);
-      });
+      createArticleListItem(articles);
     });
 };
 
