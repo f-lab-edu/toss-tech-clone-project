c9f89e3fc20c226844d58fef3eda85cdff93a578
ohgus <123801385+ohgus@users.noreply.github.com>
2024-05-16 17:34:17 +0900

    [test]: route에 param이 있는 경우 param이 제대로 추출되는지 확인하는 테스트 추가 (#14)

--- src/router/router.test.js
+++ src/router/router.test.js
@@ -1,10 +1,23 @@
-import { extractUrlParams } from './router';
+import { extractUrlParams } from './router.js';
 
 describe('router unit tests', () => {
   test('extractUrlParams test 현재 라우트에 param이 없는 경우', () => {
     const route = { params: [] };
     const pathname = '/';
     const expectValue = {};
+
+    const newParams = extractUrlParams(route, pathname);
+
+    expect(newParams).toEqual(expectValue);
+  });
+
+  test('extractUrlParams test 현재 라우트에 param이 있는 경우', () => {
+    const route = {
+      testRegExp: /^\/test\/([^\\/]+)$/,
+      params: ['id'],
+    };
+    const pathname = '/test/12343';
+    const expectValue = { id: '12343' };
 
     const newParams = extractUrlParams(route, pathname);
 
